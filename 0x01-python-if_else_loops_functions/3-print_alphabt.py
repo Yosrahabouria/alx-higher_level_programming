9598fba6b26dfdcb2830de3cd529da48d5ff6274
Yosrahabouria <yosrahabouria@gmail.com>
2023-11-28 10:11:47 -0800

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """prints the ASCII alphabet, in lowercase, not followed by a new line"""
-for l in range(97, 123):
-    if chr(l) != 'q' and chr(l) != 'e':
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(l)), end="")
