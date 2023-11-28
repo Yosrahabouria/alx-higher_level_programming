5eba64708a4fce6f65595b1c9e706a08d90338ac
Yosrahabouria <yosrahabouria@gmail.com>
2023-11-28 08:52:41 -0800

    a

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""program  prints the ASCII alphabet, in lowercase, not followed by a new line"""
+"""prints the ASCII alphabet,in lowercase,not followed by a new line"""
 
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
