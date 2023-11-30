d368032977f944c7729bfabcbfd3dcffc9fe762e
Yosrahabouria <yosrahabouria@gmail.com>
2023-11-30 06:58:18 -0800

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arglen = len(sys.argv)
-    if arglen == 1:
-        print("{} argument".format(arglen - 1))
-    elif arglen == 2:
-        print("{} argument".format(arglen - 1))
-    else:
-        print("{} argument".format(arglen - 1))
-        
-        for i in range(1, arglen)
-        print("{}:{}
+    
+    l =len(sys.argv) - 1
 
+    if l == 0:
+        print("{} argument".format(l))
+    elif l == 1:
+        print("{} argument".forma(l))
+    else:
+        print("{}argumet".format(l))
+
+    for j in range (l):
+        print("{} : {}".format(j + 1, sys.argv[j + 1]))
 
