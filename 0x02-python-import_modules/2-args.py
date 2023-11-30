177ecd9bfd80e62825c60ad6b2792861fb9b5aac
Yosrahabouria <yosrahabouria@gmail.com>
2023-11-30 05:46:14 -0800

    a

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arglen = len(sys.argv)
+    if arglen == 1:
+        print("{} argument".format(arglen - 1))
+    elif arglen == 2:
+        print("{} argument".format(arglen - 1))
+    else:
+        print("{} argument".format(arglen - 1))
+        
+        for i in range(1, arglen)
+        print("{}:{}
+
+
