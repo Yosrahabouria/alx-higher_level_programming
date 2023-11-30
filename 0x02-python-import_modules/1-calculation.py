d05861832d1d1da4c9540e01d453cd36d6ed2830
Yosrahabouria <yosrahabouria@gmail.com>
2023-11-30 05:33:17 -0800

    a

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""print the result of each function"""
 from calculator_1 import add, sub, mul, div
+"""print the result of each function"""
 if __name__ == "__main__":
     a = 10
     b = 5
