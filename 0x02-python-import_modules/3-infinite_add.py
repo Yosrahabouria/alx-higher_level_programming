72f7a1cc6661dee0dca92c3d10497e5765001a3c
Yosrahabouria <yosrahabouria@gmail.com>
2023-11-30 06:46:00 -0800

    a

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     """print infinite adds of arguments"""
     import sys
     s = 0
-    l = len(sys.argv)
-    for i in range(1, l):
-        s += int(sys.argv[i])
+    l = len(sys.argv) - 1
+    for i in range(l):
+        s += int(sys.argv[i + 1])
         print("{}".format(s))
