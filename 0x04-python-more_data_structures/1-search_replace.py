f5cf6249892cd980f95ce9a889dfe4f4f028467a
Yosrahabouria <yosrahabouria@gmail.com>
2023-12-05 23:02:28 -0800

    a

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in my_list:
+        if i == search:
+            return (replace)
+        else:
+            return (i)
