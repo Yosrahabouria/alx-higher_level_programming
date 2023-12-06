2a6199ad365313afd2c6fd46323f7bd46d6d41ab
Yosrahabouria <yosrahabouria@gmail.com>
2023-12-06 08:11:34 -0800

    a

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     """ prints the result of the addition of all arguments"""
     import sys
- 
     s = 0
     for i in range(len(sys.argv) - 1):
         s += int(sys.argv[i + 1])
