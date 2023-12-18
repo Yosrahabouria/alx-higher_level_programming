568fedd431472aafff9900121dfb75b8aa487110
Yosrahabouria <yosrahabouria@gmail.com>
2023-12-18 08:36:13 -0800

    a

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    r = 0
+    result = 0
 
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            r += a**b / i
+            result += a ** b / i
         except Exception:
-            r = b + a
+            result = b + a
             break
-        return r
+        return result
