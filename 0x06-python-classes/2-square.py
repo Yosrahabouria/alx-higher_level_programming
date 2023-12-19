c2ba18ce4c6916b2c3e7660660efa3b3469b499d
Yosrahabouria <yosrahabouria@gmail.com>
2023-12-19 06:02:17 -0800

    a

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,14 +7,15 @@ class Square:
 
     def __init__(self, size=0):
         """Instantiation with optional size.
-        
+
 
         Args:
             size: size of square
         """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if (size < 0):
-        raise TypeError("size must be >= 0")
-    else:
-        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
