8caba6375dca47bd62dc0e04cf8eff0d82b76b53
Yosrahabouria <yosrahabouria@gmail.com>
2023-12-19 04:59:41 -0800

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """Define Square"""
+
+    def __init__(self, size):
+        """New instance of square.
+
+        Args:
+          size: Square size
+        """
+        self.__size = size
