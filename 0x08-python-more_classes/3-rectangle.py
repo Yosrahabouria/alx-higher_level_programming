9650a1e8359564a581c95e550e3ad9b949d0fbac
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-04 11:03:49 -0800

    a

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,9 +51,8 @@ class Rectangle:
 
     def __str__(self):
         """return string"""
-        str1 = " "
+        str1 = ""
         if self.__width != 0 and self.__height != 0:
             str1 += "\n".join("#" * self.__width
                     for a in range(self.__height))
         return str1
-
