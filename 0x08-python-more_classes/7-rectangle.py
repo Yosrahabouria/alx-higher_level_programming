a7960905bd3e436f32e8fcd817d5c74f36bc1970
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-05 06:49:30 -0800

    a

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     """Definition of a rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """intialization of rectangle"""
