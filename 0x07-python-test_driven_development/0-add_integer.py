6468ae420a5dd55f9ee85cb73f967459f9f423be
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-07 06:56:22 -0800

    a

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """adds 2 integers."""
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a+b)
