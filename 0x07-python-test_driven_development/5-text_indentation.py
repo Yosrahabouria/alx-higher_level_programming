d209b84943ca4c387e5e4c2ce9200d6791572f8b
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-07 07:43:58 -0800

    a

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """prints a text with 2 new lines after characters"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for a in ".,?":
+        text = (a + "\n\n").join(
+            [b.strip("") for b in text.split(a)])
+
+    print(text, end="")
