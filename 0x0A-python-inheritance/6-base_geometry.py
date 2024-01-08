550621ccbdc42673a124c4ea988a9fa83d36d155
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-08 13:49:52 -0800

    a

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry method."""
+
+
+class BaseGeometry:
+    """a BaseGeometry empty class"""
+    def area(self):
+        """raise exception when message appear"""
+        raise Exception("area() is not implemented")
