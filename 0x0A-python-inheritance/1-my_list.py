a7b6543129e6d88002476a80ce823a52d4a28254
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-08 12:49:19 -0800

    a

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for class Mylist"""
+
+
+class MyList(list):
+    """description of my class"""
+    def print_sorted(self):
+        """function to print sorted list"""
+        print(sorted(self))
