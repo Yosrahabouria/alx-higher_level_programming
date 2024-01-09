47bc4aa6981f84fe9f4d6019a1985997f69ef130
Yosrahabouria <yosrahabouria@gmail.com>
2024-01-09 05:29:44 -0800

    a

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Descriptin to_json_string function"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON presentation of an object function"""
+    return json.dumps(my_obj)
