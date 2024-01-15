834c584b3122b37b320b1719497ff24c4a819253
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-01-15 15:34:59 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """ class Rectangle that inherits from Base"""
 
-
 from models.base import Base
 
 
 class Rectangle(Base):
     """Class rectangle"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialisation"""
+    def __init__(self, width, height, pos_x=0, pos_y=0, id=None):
+        """Initialization"""
         super().__init__(id)
         self.width = width
         self.height = height
-        self.x = x
-        self.y = y
+        self.pos_x = pos_x
+        self.pos_y = pos_y
 
     @property
     def width(self):
-        """return width"""
+        """Return width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set width with validation"""
         if type(value) == int:
             if value <= 0:
                 raise ValueError("width must be > 0")
@@ -32,11 +32,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """return height"""
+        """Return height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height with validation"""
         if type(value) == int:
             if value <= 0:
                 raise ValueError("height must be > 0")
@@ -45,52 +46,54 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
 
     @property
-    def x(self):
-        """return x """
-        return self.__x
+    def pos_x(self):
+        """Return x position"""
+        return self.__pos_x
 
-    @x.setter
-    def x(self, value):
+    @pos_x.setter
+    def pos_x(self, value):
+        """Set x position with validation"""
         if type(value) == int:
             if value < 0:
                 raise ValueError("x must be >= 0")
-            self.__x = value
+            self.__pos_x = value
         else:
             raise TypeError("x must be an integer")
 
     @property
-    def y(self):
-        """return y"""
-        return self.__y
+    def pos_y(self):
+        """Return y position"""
+        return self.__pos_y
 
-    @y.setter
-    def y(self, value):
+    @pos_y.setter
+    def pos_y(self, value):
+        """Set y position with validation"""
         if type(value) == int:
             if value < 0:
                 raise ValueError("y must be >= 0")
-            self.__y = value
+            self.__pos_y = value
         else:
             raise TypeError("y must be an integer")
 
     def area(self):
-        """description area of the Rectangle"""
+        """Calculate area of the Rectangle"""
         return self.width * self.height
 
     def display(self):
         """Print the Rectangle with '#' characters"""
-        for _ in range(self.__y):
+        for _ in range(self.__pos_y):
             print()
         for _ in range(self.__height):
-            print(' ' * self.__x + '#' * self.__width)
+            print(' ' * self.__pos_x + '#' * self.__width)
 
     def __str__(self):
-        """description string object of the class Rectangle instance"""
+        """Return a string representation of the class Rectangle instance"""
         return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
-                                                self.id, self.x, self.y,
-                                                self.width, self.height)
+                                                 self.id, self.pos_x, self.pos_y,
+                                                 self.width, self.height)
 
     def update(self, *args, **kwargs):
-        """Assigns an argument to attribute """
+        """Assign values to attributes"""
         if len(args) != 0:
             for i, arg in enumerate(args):
                 if i == 0:
@@ -100,14 +103,15 @@ class Rectangle(Base):
                 elif i == 2:
                     self.height = arg
                 elif i == 3:
-                    self.x = arg
+                    self.pos_x = arg
                 elif i == 4:
-                    self.y = arg
+                    self.pos_y = arg
         if kwargs is not None and len(args) == 0:
             for key, value in kwargs.items():
                 self.__setattr__(key, value)
 
     def to_dictionary(self):
-        """ dictionary description of instance of class """
-        keys = ["id", "width", "height", "x", "y"]
+        """Return dictionary description of the instance of class Rectangle"""
+        keys = ["id", "width", "height", "pos_x", "pos_y"]
         return {a: getattr(self, a) for a in keys}
+
