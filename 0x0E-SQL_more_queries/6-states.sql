dc7b0d5d0ed0d96684006d9e295b185d694bbd07
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-14 12:42:20 -0800

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 /* create the table states */
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL),
+	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id));
