0aeed6a4c80ed06a8c704135efe3a2a944df8260
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-14 12:27:48 -0800

    sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 /* creating user */
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 /* Setting the password to the user */
 /*SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';*/
