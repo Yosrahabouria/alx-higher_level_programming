fa63effe342879e1a23635e400a989f4c8a90aa9
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-14 12:25:21 -0800

    sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 /* creating user */
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 /* Setting the password to the user */
-SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
+/*SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';*/
 
 /* Giving the user select privileges on your MySQL server */
 GRANT SELECT
