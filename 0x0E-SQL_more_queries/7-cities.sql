11a3cdffbeb9e1c229bfe45d66186f6e844b33c6
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-14 13:34:10 -0800

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	PRIMARY KEY (id), 
 	id INT AUTO_INCREMENT, 
 	state_id INT NOT NULL, 
-	name VARCHAR(256) NOT NULL, 
+	name VARCHAR(256), 
+	
 	FOREIGN KEY (state_id)
 	       REFERENCES hbtn_0d_usa.states(id));
 
