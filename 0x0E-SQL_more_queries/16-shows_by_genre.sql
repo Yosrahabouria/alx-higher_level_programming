9d23a85d7a68864cf53386b544cac1a65c836805
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-14 14:33:54 -0800

    sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,10 +2,10 @@
 SELECT tv_genres.name AS name, tv_shows.title AS title
 FROM tv_shows
 
-LEFT OUTER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 
-LEFT OUTER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 
 ORDER BY name ASC, BY title ASC;
