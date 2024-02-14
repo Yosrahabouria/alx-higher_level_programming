7dfdc72d63fcf977e84b617d87ca0540c84bf080
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-02-14 20:15:00 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 --  creates the MySQL server user user_0d_1
 /* creating user*/
-CREATE USER IF NOT EXISTS user_0d_1@Llocalhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
 /* Setting the password to the user */
-SET PASSWORD FOR user_0d_1 = 'user_0d_1_pwd';
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
 /*Giving the user all privileges on your MySQL server */
 GRANT ALL PRIVILEGES
 ON *.*
