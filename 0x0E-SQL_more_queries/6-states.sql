d63e8bf8d1db823fc517e15d7b97f98ffdb97c18
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-02-14 20:35:54 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa
-/*Creating Database*/
-CREATE IF NOT EXISTS hbtn_0d_usa;
+/* Creating Database */
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 /* create the table states */
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	PRIMARY KEY id,
