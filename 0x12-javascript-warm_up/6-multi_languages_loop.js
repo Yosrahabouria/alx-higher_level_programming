439efa92bacf815594046b909e18974bbe7ca5a9
Yosrahabouria <yosrahabouria@gmail.com>
2024-03-11 09:27:41 -0700

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,6 +4,6 @@ const Myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 
 while (i < 3) {
-	console.log(Myarray[i]);
-	i++;
+  console.log(Myarray[i]);
+  i++;
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ let Myarg = parseInt(process.argv[2]);
 if (process.argv[2] === undefined || isNaN(Myarg)) {
   console.log('Missing number of occurrences');
 } else {
-	while (Myarg > 0) {
-		console.log('C is fun');
-		Myarg--;
-	}
+  while (Myarg > 0) {
+    console.log('C is fun');
+    Myarg--;
+  }
 }
