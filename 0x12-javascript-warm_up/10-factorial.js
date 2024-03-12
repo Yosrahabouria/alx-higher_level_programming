3480c366e292d1a0bec1eadbb9b20083721a4f85
Yosrahabouria <yosrahabouria@gmail.com>
2024-03-12 03:46:41 -0700

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 function factorial (x) {
   if (x <= 0){
-	  return 0;
+    return 0;
   } else if (x === 1) {
-	  return 1;
+    return 1;
   } else {
-	  return (x * factorial(x - 1));
+    return (x * factorial(x - 1));
   }
 }
 
