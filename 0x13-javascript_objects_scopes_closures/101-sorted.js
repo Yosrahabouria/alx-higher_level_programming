a71c49d8509b9702d39044709045945b28c867af
Yosrahabouria <yosrahabouria@gmail.com>
2024-03-13 04:47:13 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let Dict2 = {};
+let newDict = {};
 for (let key in dict) {
-  if (Dict2[dict[key]] === undefined) {
+  if (newDict[dict[key]] === undefined) {
     Dict2[dict[key]] = [key];
   } else {
-    Dict2[dict[key]].push(key);
+    newDict[dict[key]].push(key);
   }
 }
-console.log(Dict2);
+console.log(newDict);
