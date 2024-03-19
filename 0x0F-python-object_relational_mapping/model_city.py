1890d75ca04590e411ab74e63b19d588f2fe300c
Yosrahabouria <yosrahabouria@gmail.com>
2024-03-19 06:51:29 -0700

    python

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,7 +8,7 @@ from model_state import Base
 
 class City(Base):
     """ class defintion """
-    table = 'cities'
+   __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 class State(Base):
     """ class Definition """
-    table = 'states'
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
