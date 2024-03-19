e6277eba8db755924fe85e16382806db5e505731
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-03-19 14:59:12 +0100

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     S = sessionmaker(bind=engine)
-    s = Session()
+    s = S()
 
     my_state = s.query(State).get(2)
     my_state.name = "New Mexico"
