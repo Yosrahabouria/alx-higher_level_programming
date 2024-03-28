5955344e1675017430e377e2c9dc4408884517c5
Yosrahabouria <MY_NAME@example.com>
2024-03-28 05:54:29 -0700

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """find peak definition"""
+    if not list_of_integers:
+        return None
+
+    l = 0
+    r = len(list_of_integers) - 1
+
+    while l < r:
+        mi = (l + r) // 2
+        if list_of_integers[mi] < list_of_integers[mi + 1]:
+            l = mi + 1
+        else:
+            r = mi
+
+    return list_of_integers[l]
