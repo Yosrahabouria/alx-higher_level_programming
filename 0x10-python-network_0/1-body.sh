43e378c29b665c1d41502d246df047f8599c8ef4
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-03-28 14:08:00 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# takes a URL, sends a GET request to the URL, and displays the body of the response
+# takes a URL, sends a GET request to the URL,
+#and displays the body of the response
 curl -sL "$1"
