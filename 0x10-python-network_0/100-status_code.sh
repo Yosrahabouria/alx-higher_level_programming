f2f39b122c1c4526f4db98b8e80db7a1de294442
Yosrahabouria <MY_NAME@example.com>
2024-03-28 06:14:38 -0700

    update

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#script that sends a request to a URL passed as an argument, and displays only the status code of the response
+#script that sends a request to a URL passed as an argument,
+#and displays only the status code of the response
 curl -s "$1" -o /dev/null -w "%{http_code}"
