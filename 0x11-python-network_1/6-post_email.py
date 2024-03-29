9c0bac5aaa8c08da503b0926ba337f3865a956e9
Yosrahabouria <MY_NAME@example.com>
2024-03-29 06:22:47 -0700

    python

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Python script """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    rs = requests.post(sys.argv[1], info={'email': sys.argv[2]})
+    print(rs.text)
