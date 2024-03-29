4ae5bb88931da585c804dfea8901187a1b15365f
Yosrahabouria <MY_NAME@example.com>
2024-03-29 06:32:46 -0700

    python/json

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == '__main__':
+    URL = 'https://api.github.com/user'
+    rs = requests.get(URL, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    rs = requests.get(URL, auth=(sys.argv[1], sys.argv[2]))
+    try:
+        print(rs.json()['id'])
+    except:
+        print('None')
