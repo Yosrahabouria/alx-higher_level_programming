dd9865f4e323e68bd192f595849a7f3b467dbd43
Yosrahabouria <MY_NAME@example.com>
2024-03-29 06:04:56 -0700

    correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     rq = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(rq) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as er:
         print('Error code:', er.code)
