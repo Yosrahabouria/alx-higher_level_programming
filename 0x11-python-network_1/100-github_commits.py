2d940aeec0de016f72ff60185c72a085ea5a3e72
Yosrahabouria <MY_NAME@example.com>
2024-03-29 06:43:47 -0700

    challenge

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes 2 arguments to solve the
+challenge"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    URL = 'https://api.github.com/repos/' + sys.argv[2] + '/' + sys.argv[1] + \
+          '/commits'
+    rs = requests.get(URL)
+    try:
+        for j in range(10):
+            print('{}: {}'.format(rs.json()[j]['sha'],
+                                  rs.json()[j]['commit']['author']['name']))
+    except:
+        pass
