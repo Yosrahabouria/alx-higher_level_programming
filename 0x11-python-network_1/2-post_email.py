c9ebd785ee660a0a6e669b9d0a9f2bae25cbad9b
Yosrahabouria <MY_NAME@example.com>
2024-03-29 05:53:52 -0700

    update

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL sends a POST request to URL"""
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    data = 'email=' + sys.argv[2]
+    data = data.encode('ascii')
+    riq = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
