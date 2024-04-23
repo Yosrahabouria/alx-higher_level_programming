8c4d557c974a2c58d5cd86b2a20367b610b7cf0a
Yosrahabouria <MY_NAME@example.com>
2024-04-23 04:38:09 -0700

    js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2], function (er, resp, body) {
-  if (er) console.log(error);
+  if (er) console.log(er);
   else if (resp.statusCode === 200) {
     const dict = {};
     for (let i = 0; i < JSON.parse(body).length; i++) {
