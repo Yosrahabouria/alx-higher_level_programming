790d68257e4c01dc0e8239682e53fd7ddac08efd
Yosrahabouria <MY_NAME@example.com>
2024-04-23 04:34:54 -0700

    js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], function (er, resp, body) {
+  if (er) console.log(error);
+  else if (resp.statusCode === 200) {
+    const dict = {};
+    for (let i = 0; i < JSON.parse(body).length; i++) {
+      const user = JSON.parse(body)[i];
+      if (user.userId in dict && user.completed === true) {
+        dict[user.userId] += 1;
+      } else if (!(user.userId in dict) && user.completed === true) {
+        dict[user.userId] = 1;
+      }
+    }
+    console.log(dict);
+  }
+});
