5c98c6422a33793782b3ef7d84c2bfe9fcb79897
Yosrahabouria <MY_NAME@example.com>
2024-04-23 03:48:49 -0700

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (er, data) => {
-  if (er) console.log(err);
+  if (er) console.log(er);
   else console.log(data);
 });
