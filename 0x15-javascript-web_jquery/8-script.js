bf5b507bc252aa15cbd1a51202d1d08c18a9bf02
Yosrahabouria <yosrahabouria@gmail.com>
2024-05-02 11:39:19 -0700

    jquery

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,3 @@
-//  script that fetches and lists the title for  movies by using this URL:
 const $ = window.$;
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
     console.log(data);
